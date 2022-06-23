e1900e2642cdaa13e28d9e32e9a2c07a7d62d207
Al-E-00 <mazzellaalessio6@gmail.com>
2022-06-23 10:25:37 +0200

    font uploaded, hover effect on header improved

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,11 @@ export default {
 
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap";
+@import url('https://fonts.googleapis.com/css2?family=Asap+Condensed:wght@400;500;600;700&display=swap');
+
+#app {
+  font-family: 'Asap Condensed', sans-serif;
+}
 
 
 </style>
